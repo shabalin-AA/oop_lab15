e9fc77280714ba3bf30a6c9f938996c2833a902f
shabalin-AA <92672124+shabalin-AA@users.noreply.github.com>
2023-04-03 17:30:45 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿namespace lab15
+{
+    public static class Program
+    {
+        static Random rnd = new Random();
+        static int[] arr = new int[10];
+
+
+        static void ShowArr()
+        {
+            for (int t = 0; t < 1000; t++)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                    Console.Write(arr[i] + " ");
+                Console.WriteLine();
+            }
+        }
+
+
+        static int CompareAsc(int x, int y)
+        {
+            Thread.Sleep(5);
+            return x.CompareTo(y);
+        }
+
+        static void SortArrAscending()
+        {
+            Array.Sort(arr, CompareAsc);
+        }
+
+
+        static int CompareDesc(int x, int y)
+        {
+            Thread.Sleep(5);
+            return y.CompareTo(x);
+        }
+
+        static void SortArrDescending()
+        {
+            Array.Sort(arr, CompareDesc);
+        }
+
+
+        public static void Main()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(100);
+            }
+
+            Thread sortingAscThread = new Thread(SortArrAscending);
+            Thread sortingDescThread = new Thread(SortArrDescending);
+            Thread showingThread = new Thread(ShowArr);
+
+            showingThread.Start();
+            sortingAscThread.Start();
+            sortingDescThread.Start();
+        }
+    }
+}
